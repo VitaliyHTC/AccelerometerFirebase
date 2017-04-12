d731768ab92f8eb5c1493bdfb0e2af1b44e235c9
VitaliyHTC <vitaliyhtc@gmail.com>
2017-04-12 17:53:52 +0300

    SignIn action moved to LaunchActivity.

---
+++ app/src/main/java/com/vitaliyhtc/accelerometerfirebase/FileStoreActivity.java
@@ -0,0 +1,48 @@
+package com.vitaliyhtc.accelerometerfirebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import butterknife.ButterKnife;
+
+/**
+ * окей, тоді firebase, треба трохи переробити. Після логінізації екран з двома кнопками "Accelerometer" i "FileStore",
+ При кліку на першу кнопку переход на твоє завдання фактично,
+ при кліку на другу кнопку відкривається новий скрін де вибираєш файл(один або декілька) і завантажуєш їх на GoogleCloudStore
+ під час завантаження показується прогрес, завантаження автоматично на паузу і продовжується в залежності від onPause, onResume
+ завантажені файли користувача записуються в окрему вітку бази і є можливість їх переглянути і скачати
+ */
+public class FileStoreActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filestore);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ButterKnife.bind(this);
+
+
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                intent = new Intent(this, LaunchActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/vitaliyhtc/accelerometerfirebase/MainActivity.java
+++ app/src/main/java/com/vitaliyhtc/accelerometerfirebase/MainActivity.java
@@ -5,14 +5,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -20,9 +18,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
@@ -36,10 +31,7 @@ import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity
-        implements GoogleApiClient.OnConnectionFailedListener,
-        ActivityToDataFragment {
-
-    private static final String TAG = "MainActivity";
+        implements ActivityToDataFragment {
 
     private static final String KEY_IS_MAIN_SERVICE_RUNNING = "isMainServiceRunning";
 
@@ -52,8 +44,6 @@ public class MainActivity extends AppCompatActivity
     public static final String VALUE_DISPLAYED_SESSION_ITEM_KEY_OFF = "0x0FF";
 
     public static final String ANONYMOUS = "anonymous";
-
-    private GoogleApiClient mGoogleApiClient;
 
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
@@ -94,7 +84,7 @@ public class MainActivity extends AppCompatActivity
             mLastDisplayedFragment = VALUE_FRAGMENT_HISTORY;
         }
 
-        doAuth();
+        verifyAuth();
 
         if (isMyServiceRunning(MainService.class)) {
             isMainServiceRunning = true;
@@ -120,21 +110,11 @@ public class MainActivity extends AppCompatActivity
         outState.putString(KEY_DISPLAYED_SESSION_ITEM_KEY, mLastDisplayedSessionItemKey);
     }
 
-    private void doAuth(){
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API)
-                .build();
-
+    private void verifyAuth(){
         // Initialize Firebase Auth
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        if (mFirebaseUser == null) {
-            // Not signed in, launch the Sign In activity
-            startActivity(new Intent(this, SignInActivity.class));
-            finish();
-            return;
-        } else {
+        if (mFirebaseUser != null) {
             displayUserNameAndImage(mFirebaseUser);
             enableControlButtons();
         }
@@ -186,14 +166,6 @@ public class MainActivity extends AppCompatActivity
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
-        // be available.
-        Log.d(TAG, "onConnectionFailed:" + connectionResult);
-        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 
 
