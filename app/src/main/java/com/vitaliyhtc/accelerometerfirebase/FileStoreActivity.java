92b7d8dd9bc6964ad567783035766c27b4d0f5e5
VitaliyHTC <vitaliyhtc@gmail.com>
2017-04-14 11:56:13 +0300

    File selection dialog added.

--- app/src/main/java/com/vitaliyhtc/accelerometerfirebase/FileStoreActivity.java
+++ app/src/main/java/com/vitaliyhtc/accelerometerfirebase/FileStoreActivity.java
@@ -1,10 +1,13 @@
 package com.vitaliyhtc.accelerometerfirebase;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,12 +15,13 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * окей, тоді firebase, треба трохи переробити.
  * +++ Після логінізації екран з двома кнопками "Accelerometer" i "FileStore",
  * +++ При кліку на першу кнопку переход на твоє завдання фактично,
- *
+ * <p>
  * TODO: 2017.04.12
  * при кліку на другу кнопку відкривається новий скрін де вибираєш файл(один або декілька) і завантажуєш їх на GoogleCloudStore
  * під час завантаження показується прогрес, завантаження автоматично на паузу і продовжується в залежності від onPause, onResume
@@ -25,6 +29,7 @@ import butterknife.ButterKnife;
  */
 public class FileStoreActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_SELECT_FILE = 0x0010;
 
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
@@ -44,7 +49,6 @@ public class FileStoreActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         Intent intent;
@@ -60,12 +64,34 @@ public class FileStoreActivity extends AppCompatActivity {
     }
 
 
-
-    private void init(){
+    private void init() {
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
 
         mStorage = FirebaseStorage.getInstance();
         mStorageReference = mStorage.getReference();
+    }
+
+    @OnClick(R.id.tools_ib_upload_file)
+    void uploadFile() {
+        Intent intent;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        } else {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+        }
+        intent.setType("*/*");
+        startActivityForResult(intent, REQUEST_CODE_SELECT_FILE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_SELECT_FILE) {
+            Uri uri = data.getData();
+            Toast.makeText(this, "URI: "+uri, Toast.LENGTH_LONG).show();
+        }
     }
 }
